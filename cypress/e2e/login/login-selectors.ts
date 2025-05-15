8cfd566e4828a16b0efffd9c1bea1369fa2e4904
Maks <makzylinski@gmail.com>
2025-05-15 10:37:49 +0200

    chore: update login selectors and add initial login test structure

--- cypress/e2e/login/login-selectors.ts
+++ cypress/e2e/login/login-selectors.ts
@@ -1,5 +1,9 @@
 const loginSelectors = {
-  loginNameInput: () => cy.get('[data-testid="test]'),
+  loginNameInput: () => cy.get('[data-test-id="login-name-input"]'),
+  loginPasswordInput: () => cy.get('[data-test-id="login-password-input"]'),
+  loginShowHidePasswordButton: () =>
+    cy.get('[data-test-id="login-show-hide-password-button"]'),
+  loginSubmitButton: () => cy.get('[data-test-id="login-submit-button"]'),
 };
 
 export default loginSelectors;
